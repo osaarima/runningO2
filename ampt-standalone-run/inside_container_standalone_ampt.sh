693ebe0f930738cbbe476629c218d90c9e827b2b
osaarima <oskari.antti.matti.saarimaki@cern.ch>
2021-09-24 18:12:56 +0300

    Changed gengen directory

--- ampt-standalone-run/inside_container_standalone_ampt.sh
+++ ampt-standalone-run/inside_container_standalone_ampt.sh
@@ -10,7 +10,7 @@ alienv setenv O2/latest-dev-o2,AliRoot/latest-master-o2 -c root -b -q -l "${ampt
 
 #rm ${amptTempDir}/ana/ampt.dat
 
-alienv setenv O2/latest-dev-o2,AliRoot/latest-master-o2 -c o2-sim -j 1 --seed ${seed} -n ${Nevt} -m FV0 FT0 ITS PIPE MFT -g external --configKeyValues 'GeneratorExternal.fileName=/home/osaarima/cernbox/ToyFlowHeidi/simO2/gengen.C;GeneratorExternal.funcName=gengen("../testdir/ampt-output00.root", "ampt")'
+alienv setenv O2/latest-dev-o2,AliRoot/latest-master-o2 -c o2-sim -j 1 --seed ${seed} -n ${Nevt} -m FV0 FT0 ITS PIPE MFT -g external --configKeyValues 'GeneratorExternal.fileName=/projappl/project_2003583/simO2/runningO2/ampt-standalone-run/gengen.C;GeneratorExternal.funcName=gengen("../testdir/ampt-output00.root", "ampt")'
 
 alienv setenv O2/latest-dev-o2,AliRoot/latest-master-o2 -c o2-sim -j 1 --seed ${seed} -n ${evt} -m FV0 FT0 ITS PIPE MFT -g external --configKeyValues 'GeneratorExternal.fileName=/alicesw/O2/run/SimExamples/AliRoot_AMPT/aliroot_ampt.macro;GeneratorExternal.funcName=ampt(5500., 6.99, 8.56, 4 )'
 
